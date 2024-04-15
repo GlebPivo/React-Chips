68642b28dec3ade3f749805e95a6f7183833d4d3
Gleb_SGD <smirnovgleb541@gmail.com>
2024-04-15 15:57:36 +0300

    all

---
+++ image/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import png from './чипсыс.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={png} />
+    </div>
+  );
+}
+
+export default App;
